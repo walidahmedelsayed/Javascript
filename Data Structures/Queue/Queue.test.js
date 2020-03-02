b08b74747e609558166c2d2e364daf75148668f2
Walid <walidahmedelsayed1@gmail.com>
2020-03-02 23:53:13 +0200

    Configuring my email

--- Data Structures/Queue/Queue.test.js
+++ Data Structures/Queue/Queue.test.js
@@ -23,7 +23,7 @@ describe('Queue Test', () => {
         q.push(2);
         q.push(3);
         q.push(4);
-        q.push(5);
+        q.push(6);
         q.pop();
         expect(q.getLength()).toBe(4);
     })
