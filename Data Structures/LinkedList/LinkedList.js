5aa15cfbb5660880fb1622da3d4046fe8d8fdefe
WalidAhmed <walidahmedelsayed1@gmail.com>
2020-03-04 17:16:23 +0200

    Adding to functions to LinkedList

--- Data Structures/LinkedList/LinkedList.js
+++ Data Structures/LinkedList/LinkedList.js
@@ -4,6 +4,7 @@ class LinkedList {
 
     constructor() {
         this.head = null;
+        this.size = 0;
     }
 
     addNode(data) {
@@ -11,16 +12,66 @@ class LinkedList {
 
         if (!this.head) {
             this.head = newNode;
+            this.size++;
+            return this.head;
         }
 
         let tail = this.head;
-
         while (tail.next !== null) {
             tail = tail.next;
         }
         tail.next = newNode;
+        this.size++;
+        return this.head;
+    }
 
-        return newNode;
+    getSize() {
+        return this.size;
+    }
+
+    insertNodeAt(data, index) {
+
+        let newNode = new Node(data);
+        if (index === 0) {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        let counter = 0
+
+        let tail = this.head;
+        while (tail.next) {
+            if (counter === index) {
+                newNode.next = tail.next;
+                tail.next = newNode;
+            }
+            tail = tail.next
+            counter++;
+        }
+        return this.head;
+    }
+
+    getNodeAt(index) {
+        if (index > this.size) {
+            return new Error("The List doesn't have this index ..")
+        }
+
+        let count = 0;
+        let newNode = this.head;
+        while (newNode.next) {
+            if (index === count) {
+                return newNode;
+            }
+            newNode = newNode.next;
+            count++;
+        }
+    }
+
+    printList() {
+        let node = this.head;
+        while (node) {
+            console.log(node.data);
+            node = node.next;
+        }
     }
 }
 
--- Data Structures/LinkedList/LinkedList.test.js
+++ Data Structures/LinkedList/LinkedList.test.js
@@ -2,13 +2,59 @@ const LinkedList = require("./LinkedList");
 
 describe('LinkedList Test', () => {
 
-
-    test('Creating a LinkedList with head node', () => {
+    test('Creating a LinkedList and adding a head node', () => {
         let list = new LinkedList();
         list.addNode("Walid");
-        console.log(list);
-        list.addNode("Ahmed");
-        console.log(list);
+        expect(list.head.data).toBe("Walid");
     })
 
+    test('Creating a LinkedList and adding two nodes', () => {
+        let list = new LinkedList();
+        list.addNode("Walid");
+        list.addNode("Ahmed");
+        expect(list.head.next.data).toBe("Ahmed");
+    })
+
+    test("Check LinkedList Length", () => {
+        let list = new LinkedList();
+        list.addNode("Walid");
+        list.addNode("Ahmed");
+        list.addNode("Elsayed");
+        list.addNode("Ahmed");
+
+        expect(list.getSize()).toBe(4);
+    })
+
+
+    test("Insert Node at specific position", () => {
+        let list = new LinkedList();
+        list.addNode("Walid");
+        list.addNode("Ahmed");
+        list.addNode("Elsayed");
+        list.addNode("Ahmed");
+        list.insertNodeAt("test", 2);
+        expect(list.getSize()).toBe(4);
+    })
+
+    test("Get Node from specific position", () => {
+        let list = new LinkedList();
+        list.addNode("Walid");
+        list.addNode("Ahmed");
+        list.addNode("Elsayed");
+        list.addNode("Ahmed");
+        let node = list.getNodeAt(2);
+        expect(node.data).toBe("Elsayed");
+    })
+
+    test("Test inserting and getting node by index", () => {
+        let list = new LinkedList();
+        list.addNode("Walid");
+        list.addNode("Ahmed");
+        list.addNode("Elsayed");
+        list.addNode("Ahmed");
+        list.insertNodeAt("test", 2)
+        list.printList();
+        let node = list.getNodeAt(2);
+        expect(node.data).toBe("test");
+    })
 })
